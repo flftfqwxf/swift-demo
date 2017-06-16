48c3390023c35713971f78b5385f0f5e910af481
Xianhua Lei <xianhua.lei@istuary.com>
2017-06-16 17:21:18 +0800

    demo

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'skypp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+       # Pods for Swift-MJrefresh
+      pod 'MJRefresh','~>3.1.12'
+      pod 'Alamofire','~>4.0'
+
+end
--- skypp/video/video/ViewController.swift
+++ skypp/video/video/ViewController.swift
@@ -7,77 +7,159 @@
 //
 
 import UIKit
-
+import Alamofire
+import SwiftyJSON
+import AlamofireObjectMapper
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     let listId = "list"
-    var datas=[
-        [
-            1,2,3,4
-        ],
-        [11,22,33]
-        
+    var refreshControl: UIRefreshControl?
+
+    var datas = [
+            [
+                    1, 2, 3, 4
+            ],
+            [11, 22, 33]
+
     ]
+    var count = 0
+    var list = UITableView(frame: UIScreen.main.bounds, style: .plain)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let list=UITableView(frame:UIScreen.main.bounds,style:.plain)
+
+//        Alamofire.request("http://127.0.0.1:8033/8a15fbb94471050bb46f/list").response { response in
+//            // method defaults to `.get`
+////            debugPrint(response)
+//            do {
+//                let data = try JSONSerialization.jsonObject(with: response.data!, options: JSONSerialization.ReadingOptions.allowFragments)
+//                print(response)
+//
+//            } catch {
+//            }
+//
+//        }
         list.register(UITableViewCell.self, forCellReuseIdentifier: listId)
-        list.dataSource=self
-        list.delegate=self
-        list.backgroundColor=UIColor(red:0,green:152.0,blue:204.0,alpha:1)
-        list.separatorInset=UIEdgeInsetsMake(0, 10, 0, 50)
-        list.separatorColor=UIColor.red
-        list.separatorEffect=UIVisualEffect()
-        list.sectionHeaderHeight=150
+        list.dataSource = self
+        list.delegate = self
+        list.backgroundColor = UIColor(red: 0, green: 152.0, blue: 204.0, alpha: 1)
+        list.separatorInset = UIEdgeInsetsMake(0, 10, 0, 50)
+        list.separatorColor = UIColor.red
+        list.separatorEffect = UIVisualEffect()
+        list.sectionHeaderHeight = 150
         list.setEditing(true, animated: true)
+        refreshControl = UIRefreshControl()
+        refreshControl!.addTarget(self, action: #selector(onPullToFresh), for: UIControlEvents.valueChanged)
+        refreshControl!.attributedTitle = NSAttributedString(string: "下拉刷新")
+        list.addSubview(refreshControl!)
         view.addSubview(list)
+//        onPullToFresh()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    func onPullToFresh() {
+        refreshControl!.attributedTitle = NSAttributedString(string: "松开刷新")
+        Alamofire.request("http://127.0.0.1:8033/8a15fbb94471050bb46f/list").responseJSON { response in
+            debugPrint(response)
+
+            if let json = response.data {
+                var data = JSON(data: json)
+//                print(data["list"])
+
+//                let t = data["list"].arrayValue
+//                var t: [JSON] = data["list"].arrayValue
+//                print(t)
+                if var list = data["list"].arrayObject {
+//                    print(list)
+                    let c_data = list as! [Array<Int>]
+                    self.datas += c_data
+//                    self.datas.insert(<#T##newElement: [Int]##[Swift.Int]#>, at: <#T##Int##Swift.Int#>)
+                } else {
+                    print("error:\(data["list"])")
+                }
+                self.list.reloadData()
+                self.refreshControl!.endRefreshing()
+            }
+        }
+
+//        Alamofire.request("http://127.0.0.1:8033/8a15fbb94471050bb46f/list").response { response in
+//            // method defaults to `.get`
+////            debugPrint(response)
+//            do {
+//                let data = try JSONSerialization.jsonObject(with: response.data!, options: JSONSerialization.ReadingOptions.allowFragments)
+//                datas=(data as AnyObject).list!
+//                print(response)
+//                self.list.reloadData()
+//
+//                self.refreshControl!.endRefreshing()
+//
+//            } catch {
+//            }
+//
+//        }
+//        var temp = [Int]()
+//        for _ in 0..<5 {
+//            temp.append(Int(arc4random() % 1000))
+//        }
+//        datas.insert(temp, at: 0)
+
+
+    }
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return datas.count;
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return datas[section].count
     }
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UIScreen.main.bounds.height/3
+        return UIScreen.main.bounds.height / 3
     }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: listId, for: indexPath)
 //        let cell = tableView.dequeueReusableCell(withIdentifier: listId)
-        print(indexPath)
+//        print(indexPath)
         cell.textLabel!.text = String(datas[indexPath.section][indexPath.row])
         return cell
     }
-    func tableView(_ tableView:UITableView,commit editingStyle:UITableViewCellEditingStyle,forRowAt indexPath:IndexPath){
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             datas[indexPath.section].remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.fade)
-        }else if editingStyle == .insert {
-            datas[indexPath.section].insert(77,at:indexPath.row+1)
-            let zyIndexPath = IndexPath(row: indexPath.row+1, section: indexPath.section)
+        } else if editingStyle == .insert {
+            datas[indexPath.section].insert(77, at: indexPath.row + 1)
+            let zyIndexPath = IndexPath(row: indexPath.row + 1, section: indexPath.section)
 
             tableView.insertRows(at: [zyIndexPath], with: UITableViewRowAnimation.middle)
         }
     }
+
     func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
         return "删除"
     }
+
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return " this is header"
     }
+
     func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         return "this is footer"
     }
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("this is click event \(datas[indexPath.section][indexPath.row])")
     }
+
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        
+
     }
+
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
-        
+
         if indexPath.row % 2 == 0 {
             return UITableViewCellEditingStyle.insert
 
@@ -86,6 +168,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
 
     }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
